70691fa7fded0ea0e44307e4e8251f6d30c6e348
Jordan <jcharrison12@gmail.com>
2021-07-21 09:56:36 -0500

    Adds all five CRUD actions and happy sad paths

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create!(name: "Disney World", address: "Orlando, FL")
+Place.create!(name: "Sears Tower", address: "233 S Wacker Dr, Chicago, IL 60606")
+Place.create!(name: "Metropolis Coffee Company", address: "1039 W Granville Ave, Chicago, IL 60660")
